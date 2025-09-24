055b6bda3b91a2a52d2da46986158b0c89b0f3be
KDizzle <99917328+Kdizzleva@users.noreply.github.com>
2025-09-24 00:13:38 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,8 @@
-/* LG2 Site Config
-   Central place for site-wide constants
-   Any page can import this before its own script
-*/
-
+/* LG2 Site Config */
 window.LG2_CONFIG = {
-  // Google Sheets CSV (videos)
   VIDEOS_CSV_URL:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSziN8EwaEBgx5c1BtKRThZycM8JDjxISm_Irsr-ji0eKGoO0POxS2L5rJUUMY496fAO3rm8GutyyTo/pub?gid=0&single=true&output=csv",
-
-  // (optional) Blog Sheet CSV (set if you want blog.js to use it)
-  BLOG_CSV_URL: "",
-
-  // Social links
+  BLOG_CSV_URL: "", // set later if you want blog posts from a Sheet
   SOCIALS: {
     youtube:  "https://www.youtube.com/@Kdizzleva",
     instagram:"https://www.instagram.com/letsgoto.yt/",
